98ea0425bd0ac31f78cc97e6b5ca90e7a6817f41
Amin El Gazzar <113841191+Amin-ElGazzar@users.noreply.github.com>
2024-09-24 19:43:34 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { BrandsComponent } from './brands/brands.component';
+import { CartComponent } from './cart/cart.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    BrandsComponent,
+    CartComponent,
+    CategoriesComponent,
+    FooterComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    NotfoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
