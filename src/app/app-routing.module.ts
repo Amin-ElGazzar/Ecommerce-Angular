15a324746e431f318a79877c99b6b521423d5251
Amin El Gazzar <113841191+Amin-ElGazzar@users.noreply.github.com>
2024-09-24 20:12:01 +0300

    handle routing in project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CartComponent } from './cart/cart.component';
+import { BrandsComponent } from './brands/brands.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home' ,component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'cart',component:CartComponent},
+  {path:'brands',component:BrandsComponent},
+  {path:'categories',component:CategoriesComponent},
+  {path:'footer',component:FooterComponent},
+  {path:'login',component:LoginComponent},
+  {path:'navbar',component:NavbarComponent},
+  {path:'**',component:NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
