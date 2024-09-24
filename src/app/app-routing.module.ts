25ddb0575901661791951eb367808abdbb54ccae
Amin El Gazzar <113841191+Amin-ElGazzar@users.noreply.github.com>
2024-09-24 21:36:52 +0300

    handle navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,22 +9,22 @@ import { FooterComponent } from './footer/footer.component';
 import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NotfoundComponent } from './notfound/notfound.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path:'',redirectTo:'home',pathMatch:'full'},
-  {path:'home' ,component:HomeComponent},
-  {path:'about',component:AboutComponent},
-  {path:'cart',component:CartComponent},
-  {path:'brands',component:BrandsComponent},
-  {path:'categories',component:CategoriesComponent},
-  {path:'footer',component:FooterComponent},
-  {path:'login',component:LoginComponent},
-  {path:'navbar',component:NavbarComponent},
-  {path:'**',component:NotfoundComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'brands', component: BrandsComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotfoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
